c136c197facc7cb1ae8754ff418f0ae8eac6eba6
Nikola-Mircic <nikola123mircic@gmail.com>
2025-09-25 00:58:23 +0900

    Tip entiteta

---
+++ NetworkService/NetworkService/NetworkService/Model/DistEnergyResource.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Model
+{
+    public class DistEnergyResource
+    {
+        int id = -1;
+        private String name = "";
+        private DistEnergyResourceType type;
+        private double value = 0.0;
+
+        public DistEnergyResource(int id, string name, DistEnergyResourceType type, double value)
+        {
+            this.id = id;
+            this.name = name;
+            this.type = type;
+            this.value = value;
+        }
+
+        public String Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public double Value
+        {
+            get { return this.value; }
+            set { this.value = value; }
+        }
+
+        public DistEnergyResourceType Type
+        {
+            get { return this.type; }
+            set { this.type = value; }
+        }
+
+        public int ID
+        {
+            get { return this.id; }
+        }
+    }
+}
---
+++ NetworkService/NetworkService/NetworkService/Model/DistEnergyResourceType.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Model
+{
+    public class DistEnergyResourceType
+    {
+        public static DistEnergyResourceType SOLAR = new DistEnergyResourceType("Solar", "");
+        public static DistEnergyResourceType WIND = new DistEnergyResourceType("Wind", "");
+
+        private String name = "";
+        private String image = "";
+
+        private DistEnergyResourceType(string name, string image)
+        {
+            this.name = name;
+            this.image = image;
+        }
+
+        public String Name
+        {
+            get { return this.name; }
+            set { this.name = value; }  
+        }
+
+        public String Image
+        {
+            get { return this.image; }
+            set { this.image = value; }
+        }
+    } 
+}
